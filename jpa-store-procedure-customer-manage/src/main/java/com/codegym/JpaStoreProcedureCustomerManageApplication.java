d5e8d7309cad3dcbe9b12299b2a57d62c9bac916
Ngo Hoang <ngohoang8320@gmail.com>
2024-04-15 12:08:48 +0700

    Add exercise project about manage products

---
+++ jpa-store-procedure-customer-manage/src/main/java/com/codegym/JpaStoreProcedureCustomerManageApplication.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStoreProcedureCustomerManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaStoreProcedureCustomerManageApplication.class,
+                args);
+    }
+
+}
---
+++ jpa-store-procedure-customer-manage/src/main/java/com/codegym/repository/CustomerRepository.java
@@ -0,0 +1,26 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public class CustomerRepository implements ICustomerRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public boolean saveWithStoredProcedure(Customer customer) {
+        String sql = "CALL Insert_Customer(:firstName, :lastName)";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("firstName",
+                customer.getFirstName());
+        query.setParameter("lastName",
+                customer.getLastName());
+        return query.executeUpdate() != 0;
+    }
+}
