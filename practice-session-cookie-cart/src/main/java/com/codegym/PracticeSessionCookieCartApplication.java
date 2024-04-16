00e2731e7e36dc50c64c95403426221bb320c580
Ngo Hoang <ngohoang8320@gmail.com>
2024-04-16 21:49:04 +0700

    Add practice project about cart and shop page using session

---
+++ practice-session-cookie-cart/src/main/java/com/codegym/PracticeSessionCookieCartApplication.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSessionCookieCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSessionCookieCartApplication.class,
+                args);
+    }
+
+}
