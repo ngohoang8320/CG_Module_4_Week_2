2026f345a65ffd27bcb02fde54adc4ac8d6c862f
Ngo Hoang <ngohoang8320@gmail.com>
2024-04-16 08:32:35 +0700

    Add practice project about counter view use session

---
+++ prac-session-count-view/src/main/java/com/codegym/PracSessionCountViewApplication.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSessionCountViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracSessionCountViewApplication.class,
+                args);
+    }
+
+}
